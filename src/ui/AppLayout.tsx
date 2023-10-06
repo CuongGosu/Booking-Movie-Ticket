1328b51f05f55c5d0373a45d60b6abb432155f5a
CuongGosu <hoangngocquoccuong@gmail.com>
2023-10-07 01:33:17 +0700

    add component Footer

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 interface PropsAppLayout {}
 
 const AppLayout: React.FC<PropsAppLayout> = () => {
   return (
-    <div className="bg-main-background mx-auto min-h-full min-w-320">
+    <div className="mx-auto min-h-full min-w-320">
       <Header />
-      <main className="mt-7">
+      <main className=" mt-7 h-full w-full">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
